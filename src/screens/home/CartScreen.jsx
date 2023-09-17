3e6af89833a837950eea92dd319bd8fabb159d5e
Alejandro Torres Naranjo <124210642+i2001atnj@users.noreply.github.com>
2023-09-17 15:54:06 -0500

    changes

--- src/screens/home/CartScreen.jsx
+++ src/screens/home/CartScreen.jsx
@@ -8,15 +8,14 @@ import EmptyCartInfo from "../../components/EmptyCartInfo";
 export default function CartScreen() {
   const navigation = useNavigation();
   return (
-    <SafeAreaView style={{ backgroundColor: "#fff", height: "100%" }}>
+    <SafeAreaView style={{ backgroundColor: "#fff", height: "80%" }}>
       <Header style={{ justifyContent: "center" }} title="Shopping Cart" />
-      <View style={{ height: "80%" }}>
-        <EmptyCartInfo text="Your cart is empty !" />
-        <ButtonComponent
-          text="Start shopping"
-          buttonFunction={() => navigation.navigate("Home Screen")}
-        />
-      </View>
+      <EmptyCartInfo text="Your cart is empty !" />
+      <ButtonComponent
+        text="Start shopping"
+        buttonFunction={() => navigation.navigate("Home Screen")}
+        style={{ paddingHorizontal: 17 }}
+      />
     </SafeAreaView>
   );
 }
