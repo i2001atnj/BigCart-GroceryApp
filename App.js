81dadd9aca6a6b0059a0f5e73190ed91d1d4e941
Alejandro Torres Naranjo <124210642+i2001atnj@users.noreply.github.com>
2023-09-24 06:38:03 -0500

    changes

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { onAuthStateChanged } from "firebase/auth";
